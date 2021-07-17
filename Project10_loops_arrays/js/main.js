8805b88052725fcb897db648d32e05cf6a3847de
jasonwsvt <71233413+jasonwsvt@users.noreply.github.com>
2021-07-17 17:55:21 -0400

    Separate out whileLoop and forLoop functions

--- Project10_loops_arrays/js/main.js
+++ Project10_loops_arrays/js/main.js
@@ -1,22 +1,39 @@
+var max
 
 //Display the times table for the value in the factor input field.
-function loop() {
-	var max = parseInt(document.getElementById('factor').value)
+function createTable() {
+	max = parseInt(document.getElementById('factor').value)
 
 	var code = "<table>\n"
-	var x = 1
-	do {
-		code += "	<tr>\n"
-		for (let y = 1; y <= max; y++) {
-			code += `		<td>${ x * y }</td>\n`
-			console.log(x, y)
-		}
-		code += "	</tr>\n"
-	}
-	while (x++ < max)
+	code += whileLoop()
 	code += "</table>\n"
 	document.querySelector("div").innerHTML = code
 }
 
+//create code for every row
+function whileLoop() {
+	var code = ""
+	var x = 1
+	do {
+		code += "	<tr>\n"
+		code += forLoop(x, max)
+		code += "	</tr>\n"
+	}
+	while (x++ < max)
+
+	return code
+}
+
+//create code for a single row
+function forLoop(x) {
+	var code = ""
+	for (let y = 1; y <= max; y++) {
+		code += `		<td>${ x * y }</td>\n`
+		console.log(x, y)
+	}
+
+	return code
+}
+
 //Add an onload event listener so the default (10) times table will load automatically.
-window.addEventListener('load', loop)
+window.addEventListener('load', createTable)
